20651808a5b0c00e1ab09b704479e9f3ecfa0f0a
Sam Murphy <sam-murphy@hotmail.co.uk>
2016-10-20 00:12:48 +0100

    Projectiles, simple gun, particle effects

---
+++ Assets/Shooter/Scripts/DeathToParticles.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DeathToParticles : MonoBehaviour, IDamageable
+{
+
+    ParticleSystem.EmissionModule particles;
+    public float Health = 100;
+	// Use this for initialization
+	void Start () {
+        particles = GetComponent<ParticleSystem>().emission;
+        particles.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (particles.enabled)
+        {
+
+        }
+	}
+
+    public void Die()
+    {
+        //GetComponent<MeshRenderer>().enabled = false;
+        particles.enabled = true;
+    }
+
+    public void TakeHit(float damage, Vector3 hitPoint, Vector3 hitDirection)
+    {
+        TakeDamage(damage);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+        if (Health <= 0) Die();
+    }
+}
---
+++ Assets/Shooter/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public enum FireMode
+    {
+        Single,
+        Burst,
+        Automatic
+    }
+
+    public Projectile projectile;
+    public float MuzzleVelocity;
+    public Transform[] ProjectileSpawns;
+    public FireMode fireMode = FireMode.Single;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        Shoot();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void Shoot()
+    {
+        foreach (Transform spawn in ProjectileSpawns)
+        {
+            Projectile newProjectile = Instantiate(projectile, spawn.position, spawn.rotation) as Projectile;
+            newProjectile.SetSpeed(MuzzleVelocity);
+        }
+    }
+}
